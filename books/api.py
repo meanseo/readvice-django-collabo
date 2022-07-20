2d928cfbef791302bcba3491d868c7588dcaabad
meanseo <chlalstjok97@gmail.com>
2022-07-20 14:48:20 +0900

    book api data processing

--- books/api.py
+++ books/api.py
@@ -1,54 +1,32 @@
-import requests
 import json
-
 from icecream import ic
-
 import config
 import urllib.request
-from urllib.parse import urlencode, quote_plus, unquote
 import pandas as pd
-from pandas.io.json import json_normalize
 
-
+# 정보나루 API 및 데이터 정제
 SUBSCRIPTION_KEY = config.data_book_api_key
 # url2 = f'http://data4library.kr/api/libSrchByBook?authKey={config.data_book_api_key}&isbn=[ISBN]&region=[지역코드]'
 url = f'http://data4library.kr/api/loanItemSrch?authKey={config.data_book_api_key}&startDt=2022-07-10&endDt=2022-07-12&format=json'
 # queryParams = '&' + urlencode({quote_plus('page'): '1', quote_plus('perPage'): '1824', quote_plus('returnType'): 'JSON'})
 
 response = urllib.request.urlopen(url).read()
-
 # json_str = response.read().decode("utf-8")
-
 json_object = json.loads(response)
-
 body = json_object['response']
-
 json_object = body['docs']
-
 # del json_object['doc']
-
-ic(json_object)
-
-# print(type(json_object))
-# print(json_object[0].keys)
-
-# book_list=[]
+# ic(json_object)
 
 book_list = [json_object[i]['doc'] for i in range(0, len(json_object))]
-ic(book_list)
+# ic(book_list)
 
 df = pd.DataFrame.from_records(book_list)
-ic(df)
+# ic(df)
 
 df.drop(['addition_symbol', 'class_no', 'loan_count', 'no', 'publication_year', 'ranking', 'vol', 'publisher'], axis=1, inplace=True)
 df.columns = ['book_title', 'authors', 'isbn', 'category', 'book_img']
-df = df.to_json(orient="index")
-json_object = json.loads(df)
-a = json_object.values()
-ic(a)
-
-# df.to_csv('./save/book_list.csv', index=False)
-# dataframe = json_normalize(json_object)
-# print(dataframe)
-# file = open('book.json', "w+")
-# file.write(json.dumps(json_object))
+df2json = df.to_json(orient="index")
+df2json = json.loads(df2json)
+book_api_data = df2json.values()
+ic(book_api_data)
