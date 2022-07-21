aa33ffedccfe43594f594020971e1fc243042d52
BeautyKim <kima2012114@gmail.com>
2022-07-21 18:19:27 +0900

    server-front connect test

--- admin/views.py
+++ admin/views.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+import datetime
 
 
 @api_view(['GET'])
 def hello_api(request):
     print("####### 서버로 들어옴 #######")
-    return Response({'message': datetime.now()})
+    return Response({'message': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
